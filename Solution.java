0727b6803f22d72239b6e385146dfbc5537f530a
kirser5 <70627247+kirser5@users.noreply.github.com>
2020-09-29 17:21:33 +0300

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static void recursion() {
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        int n = in.nextInt();
+        if (n > 0) {
+            if (n % 2 == 1) {
+                System.out.println(n);
+                recursion();
+            } else {
+                recursion();
+            }
+        }
+    }
+}
